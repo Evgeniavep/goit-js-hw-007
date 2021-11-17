d06be7bde9ff76090b11a5f5c8d25e33c9533265
Evhenia Vepretskaya <enychka12@gmail.com>
2021-11-17 14:56:29 +0200

    Модалка работает

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
+
+
+const instance = basicLightbox.create(`
+    <img src="" width="800" height="600">
+`)
+
+
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`
+    }).join('');
+};
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryContainer.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+    event.preventDefault();
+    instance.show()
+
+};
